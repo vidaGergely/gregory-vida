8f693a3ebcb3058b5d31618433579a5e3086beb6
Gergely Vida <gergely.vida@tmrw.life>
2021-04-25 18:56:48 +0200

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,28 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+  if (true) {
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <div className="text-2xl animate-bounce ">
+          <Image
+            src="/uc.png"
+            width={200}
+            height={200}
+            alt="Picture of under construct"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </Image>
         </div>
-      </main>
-    </div>
-  );
+      </div>
+    );
+  }
+  else
+  {
+    return (
+      <p>itt</p>
+    );
+  }
+
+
 }
